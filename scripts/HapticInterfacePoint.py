38a3663be9ec7325c81dc5115ba7841d836d9a57
Raagini Rameshwar <rrameshwar@system76-pc.localdomain>
2022-10-12 21:59:21 -0400

    commented things and now trying to merge

--- scripts/HapticInterfacePoint.py
+++ scripts/HapticInterfacePoint.py
@@ -9,11 +9,11 @@ from checker import *
 import numpy as np
 from myObject import *
 
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-from matplotlib import style
-
 import datetime as dt
+
+## The HIP class keeps track of everything to do with the HIP, including the god object and the force output.
+## This is where we update the HIP position and also make all the calculations to update the god object position and
+## output forces. 
 
 class HapticInterfacePoint():
 	def __init__(self, model, initial_position=[0, 0, 0]):
@@ -34,11 +34,6 @@ class HapticInterfacePoint():
 
 		self.checker = CollisionChecker(self.model)
 
-		self.xs = []
-		self.ys = []
-
-		self.fig = plt.figure()
-		self.ax1 = self.fig.add_subplot(1,1,1)
 		self.isConcave = False
 
 	## This function should move the HIP and (if colliding) the God object positions
@@ -48,7 +43,8 @@ class HapticInterfacePoint():
 		self.previous_position = self.current_position
 		self.current_position = np.add(self.current_position, transformation)	
 
-		## If inside object, let's update our plane constraints iteratively
+		## If inside object, let's update our plane constraints iteratively. This also calculates a 
+		## new god object position
 		if self.inside_object:
 			self.updatePlaneConstraints()
 		else:
@@ -56,11 +52,10 @@ class HapticInterfacePoint():
 			self.god_pos_prev = self.god_pos
 			self.god_pos = self.previous_position
 			
-		
+		# Calculate and print output force		
 		self.calculateForce()
 		print("CURRENT FORCE = ", round(np.linalg.norm(self.rendered_force),3))
-		# print("HIP PREV ", self.previous_position, " HIP NOW ", self.current_position, " GOD PREV ", self.god_pos_prev, " GOD NOW ", self.god_pos, " ACTIVE PLANES ", self.active_planes)
-		
+	
 
 	## Iteratively update plane constraints. Find new constraints between hip and old god, and update god object
 	## THEN check constraints between new and old god and update god object AGAIN.
@@ -91,9 +86,9 @@ class HapticInterfacePoint():
 
 
 			## Check neighbors to see if new constraint - USING OLD GOD OBJ AND NEW GOD OBJ
+			## This might be where extra active planes get discovered
 			__, new_constraints = self.checker.detectCollision(self.possible_planes, temp_god_pos, self.god_pos_prev, True, self.isConcave)
 			new_constraints = [*set(new_constraints)] # This removes duplicates
-
 
 
 			## Combine all of our current constraints
@@ -105,44 +100,150 @@ class HapticInterfacePoint():
 			self.god_pos_prev = self.god_pos
 			self.god_pos = self.calculateGod(self.active_planes)
 
-			# print("OLD CONSTRAINTS:", old_constraints, "NEW CONSTRAINTS:", new_constraints)
-			# print("ACTIVE PLANES:", self.active_planes, "PREV ACTIVE PLANES:", self.active_planes_prev)
+
+	## Returns new calculated god object position minimizing distance b/w hip and god while maintaining constraint
+	def calculateGod(self, prim_list):
+
+		## Step 1 is to trim the list of active planes if any of them are coplanar. Coplanar primitives will
+		## give us a singular matrix A and inverting it will cause an error.
+
+		prim_list_trimmed = []
+
+		# If we only have 2, we can do a simple check with no for loops
+		if len(prim_list) == 2:
+			if self.isCoplanar(self.model.faces[prim_list[0]], self.model.faces[prim_list[1]]):
+				# print("removed a coplanar")
+				prim_list.remove(prim_list[1])
+
+		# For more than 2, we need to check all planes against all other planes (but no duplicate checking!)
+		# We keep track of all the coplanar planes we need to remove
+		if len(prim_list) >= 3:
+			to_remove = []
+			for num, prim in enumerate(prim_list):
+				for num2, prim2 in enumerate(prim_list[num+1:-1]):
+					if self.isCoplanar(self.model_faces[prim], self.model_faces[prim2]):
+						to_remove.append(num2)
+
+			## And remove the coplanar plaes
+			prim_list_trimmed = prim_list
+			for i in to_remove:
+				prim_list_trimmed.remove(prim_list[i])
 
 
-	## Optimization - check neighboring triangles (planes) that share a single point with active triangle
+		if len(prim_list_trimmed) > 0:
+			prim_list = prim_list_trimmed
+
+		# We use the trimmed primitive list (all our legitimate active planes) to calculate plane constants
+		# We will have a minimum of 1, a maximum of 3.
+		
+		consts = np.zeros([3,4])
+		for i in range(0, len(prim_list)):			
+			triangle = self.model.faces[prim_list[i]]
+			triangle_points = (self.model.vertices[triangle[0]], 
+				self.model.vertices[triangle[1]], self.model.vertices[triangle[2]]) 
+			
+			# Use the vertices of the primitive to calculate the plane constants and update "consts"
+			consts[i] = self.calcPlaneFromPrim(triangle_points)
+		
+			# consts = [a1 b1 c1 d1
+			#           a2 b2 c2 d2
+			#           a3 b3 c3 d3]
+
+
+		# Calculate A and B
+		# B is the current position of the HIP
+
+		# A = 
+		# [1  0  0  a1 a2 a3
+		#  0  1  0  b1 b2 b3
+		#  0  0  1  c1 c2 c3
+		#  a1 b1 c1  0 0  0
+		#  a2 b2 c2  0 0  0
+		#  a3 b3 c3  0 0  0 ]
+
+		# A will be 4x4 if we only have 1 constraint, 5x5 if we have 2, 6x6 if we have 3
+
+		## A failsafe for testing purposes, we should never get to this point
+		if len(prim_list) == 0:
+			A = np.eye(3)
+			B = np.array([self.current_position[0], self.current_position[1], self.current_position[2]])
+
+		## 1 plane constraint
+		if len(prim_list) == 1:
+			A = [
+				[1, 0, 0, consts[0][0]],
+				[0, 1, 0, consts[0][1]],
+				[0, 0, 1, consts[0][2]],
+				[consts[0][0], consts[0][1], consts[0][2], 0]]
+
+			B = np.array([self.current_position[0], self.current_position[1], self.current_position[2], consts[0][3]*-1])
+
+		## 2 plane constraints
+		if len(prim_list) == 2:
+			A = [
+				[1, 0, 0, consts[0][0], consts[1][0]],
+				[0, 1, 0, consts[0][1], consts[1][1]],
+				[0, 0, 1, consts[0][2], consts[1][2]],
+				[consts[0][0], consts[0][1], consts[0][2], 0, 0],
+				[consts[1][0], consts[1][1], consts[1][2], 0, 0]
+				]
+
+			B = np.array([self.current_position[0], self.current_position[1], self.current_position[2], consts[0][3]*-1, consts[1][3]*-1])
+		
+		## 3 plane constraints
+		if len(prim_list) >= 3:
+			A = [
+				[1, 0, 0, consts[0][0], consts[1][0], consts[2][0]],
+				[0, 1, 0, consts[0][1], consts[1][1], consts[2][1]],
+				[0, 0, 1, consts[0][2], consts[1][2], consts[2][2]],
+				[consts[0][0], consts[0][1], consts[0][2], 0, 0, 0],
+				[consts[1][0], consts[1][1], consts[1][2], 0, 0, 0],
+				[consts[2][0], consts[2][1], consts[2][2], 0, 0, 0]]
+
+			B = np.array([self.current_position[0], self.current_position[1], self.current_position[2], consts[0][3]*-1, consts[1][3]*-1, consts[2][3]*-1])
+
+
+		#FINALLY calculate the god object position using A and B.
+
+		x_godobj = np.dot(np.linalg.inv(A), B)
+		return np.array([x_godobj[0], x_godobj[1], x_godobj[2]])
+
+	# Returns an output force based on current HIP and god object positions
+	def calculateForce(self):
+		k = [17, 17, 17]
+		
+		for i in range(0, 3):
+			self.rendered_force[i] = [k[i]*(self.god_pos[i] - self.current_position[i])]
+
+
+	## HELPER FUNCTIONS ##
+
+	## Check neighboring triangles (planes) that share a single point with active triangle. We need to check
+	## these planes later to see if the god object has switched constraint planes.
 	def updatePossiblePlanes(self):
 		self.possible_planes = []
-		# print("\nList of active planes ", self.active_planes)
 
 		## For each active plane, find neighbors
 		for active_plane in self.active_planes:
 			active_plane_points = self.model_faces[active_plane]
-			# print("Current active plane ", active_plane, active_plane_points, [self.model.vertices[active_plane_points[0]], self.model.vertices[active_plane_points[1]], self.model.vertices[active_plane_points[2]]])
 			
 			## For each object face (must check all faces EVERY time)
 			for face in self.model_faces:
 				if (any(point in face for point in active_plane_points)):
-					# if not face in self.possible_planes: ## THIS DOESNT DO ANYTHING? Oct 11 12:04am
 					self.possible_planes.append(face) # This will include our active plane as well
 
-
+	## Takes a triangle of 3 vertices and outputs the plane equation of that triangle. Calculates 2 edges of the
+	## triangle, and takes a cross product.
 	def calcPlaneFromPrim(self, prim):
-		#prim = [[x1, y1, z1], [x2, y2, z2], [x3, y3, z3]]
+
 		x1 = prim[0][0]; y1 = prim[0][1]; z1 = prim[0][2]
 		x2 = prim[1][0]; y2 = prim[1][1]; z2 = prim[1][2]
 		x3 = prim[2][0]; y3 = prim[2][1]; z3 = prim[2][2]
 
-		# edge1 = [x2-x1, y2-y1, z2-z1]
-		# edge2 = [x3-x1, y3-y1, z3-z1]
-
 		edge1 = [np.subtract(x2,x1), np.subtract(y2,y1), np.subtract(z2,z1)]
 		edge2 = [np.subtract(x3,x1), np.subtract(y3,y1), np.subtract(z3,z1)]
 
-		#print(edge1, edge2)
-
 		cross_product = np.cross(edge1, edge2)
-
-		#print(cross_product)            
 
 		a = cross_product[0]
 		b = cross_product[1]
@@ -153,9 +254,10 @@ class HapticInterfacePoint():
 		return a, b, c, d
 
 
+	## Takes 2 triangles and returns True if they are coplanar, and False if they are not. Calculates the normal
+	## to each triangle and checks if the normals are parallel or not.
 	def isCoplanar(self, tri1, tri2):
-		# print(tri1)
-		# print(tri2)
+
 		cross1 = np.cross(np.subtract(self.model.vertices[tri1[0]], self.model.vertices[tri1[1]]), np.subtract(self.model.vertices[tri1[1]], self.model.vertices[tri1[2]]))
 		cross2 = np.cross(np.subtract(self.model.vertices[tri2[0]], self.model.vertices[tri2[1]]), np.subtract(self.model.vertices[tri2[1]], self.model.vertices[tri2[2]]))
 
@@ -166,167 +268,3 @@ class HapticInterfacePoint():
 		
 		if abs(np.linalg.norm(check_cross)) == 0.0:
 			return True
-
-
-	## Returns new calculated god object position minimizing distance b/w hip and god while maintaining constraint
-	def calculateGod(self, prim_list):
-
-		# print("OG PURE PRIM LIST:", prim_list)
-		consts = np.zeros([3,4])
-		prim_list_trimmed = []
-
-		#Trim the primitive list to delete coplanar primitives
-
-		if len(prim_list) == 2:
-			if self.isCoplanar(self.model.faces[prim_list[0]], self.model.faces[prim_list[1]]):
-				# print("removed a coplanar")
-				prim_list.remove(prim_list[1])
-
-		if len(prim_list) >= 3:
-			to_remove = []
-			for num, prim in enumerate(prim_list):
-				for num2, prim2 in enumerate(prim_list[num+1:-1]):
-					if self.isCoplanar(self.model_faces[prim], self.model_faces[prim2]):
-						to_remove.append(num2)
-						# print("FOUND SOME COPLANARS",prim, prim2)
-
-
-			# if self.isCoplanar(self.model.faces[prim_list[0]], self.model.faces[prim_list[1]]):
-			# 	to_remove.append(1)
-			# if self.isCoplanar(self.model.faces[prim_list[0]], self.model.faces[prim_list[2]]):
-			# 	to_remove.append(2)
-			# if self.isCoplanar(self.model.faces[prim_list[1]], self.model.faces[prim_list[2]]):
-			# 	to_remove.append(2)
-
-			prim_list_trimmed = prim_list
-			for i in to_remove:
-				prim_list_trimmed.remove(prim_list[i])
-
-			# print("Trimmed List:", prim_list_trimmed)
-
-		if len(prim_list_trimmed) > 0:
-			prim_list = prim_list_trimmed
-
-			
-		for i in range(0, 3):
-			try:
-				# print("TRYING SOME SHIT")
-				triangle = self.model.faces[prim_list[i]]
-				# print("Triangle ", i, " is ", triangle)
-				triangle_points = (self.model.vertices[triangle[0]], 
-					self.model.vertices[triangle[1]], self.model.vertices[triangle[2]]) 
-				
-				consts[i] = self.calcPlaneFromPrim(triangle_points)
-			except:
-				# print("NO MORE PRIMS")
-				pass
-		
-		# consts = [a1 b1 c1 d1
-		#           a2 b2 c2 d2
-		#           a3 b3 c3 d3]
-
-		# print("OG prim list (len, list)", len(prim_list), prim_list)
-
-
-			
-		# print("plane consts, ", consts)
-
-		# A = [
-		# 	[1, 0, 0, consts[0][0], consts[1][0], consts[2][0]],
-		# 	[0, 1, 0, consts[0][1], consts[1][1], consts[2][1]],
-		# 	[0, 0, 1, consts[0][2], consts[1][2], consts[2][2]],
-		# 	[consts[0][0], consts[0][1], consts[0][2], 0, 0, 0],
-		# 	[consts[1][0], consts[1][1], consts[1][2], 0, 0, 0],
-		# 	[consts[2][0], consts[2][1], consts[2][2], 0, 0, 0]]
-
-		if len(prim_list) == 0:
-			A = np.eye(3)
-			B = np.array([self.current_position[0], self.current_position[1], self.current_position[2]])
-
-		if len(prim_list) == 1:
-			A = [
-				[1, 0, 0, consts[0][0]],
-				[0, 1, 0, consts[0][1]],
-				[0, 0, 1, consts[0][2]],
-				[consts[0][0], consts[0][1], consts[0][2], 0]]
-
-			#B = np.array([self.previous_position[0], self.previous_position[1], self.previous_position[2], consts[0][3]*-1])
-			B = np.array([self.current_position[0], self.current_position[1], self.current_position[2], consts[0][3]*-1])
-
-			# print("A = ", A)
-
-		if len(prim_list) == 2:
-			A = [
-				[1, 0, 0, consts[0][0], consts[1][0]],
-				[0, 1, 0, consts[0][1], consts[1][1]],
-				[0, 0, 1, consts[0][2], consts[1][2]],
-				[consts[0][0], consts[0][1], consts[0][2], 0, 0],
-				[consts[1][0], consts[1][1], consts[1][2], 0, 0]
-				]
-
-			#B = np.array([self.previous_position[0], self.previous_position[1], self.previous_position[2], consts[0][3]*-1, consts[1][3]*-1])
-			B = np.array([self.current_position[0], self.current_position[1], self.current_position[2], consts[0][3]*-1, consts[1][3]*-1])
-		
-		if len(prim_list) >= 3:
-			A = [
-				[1, 0, 0, consts[0][0], consts[1][0], consts[2][0]],
-				[0, 1, 0, consts[0][1], consts[1][1], consts[2][1]],
-				[0, 0, 1, consts[0][2], consts[1][2], consts[2][2]],
-				[consts[0][0], consts[0][1], consts[0][2], 0, 0, 0],
-				[consts[1][0], consts[1][1], consts[1][2], 0, 0, 0],
-				[consts[2][0], consts[2][1], consts[2][2], 0, 0, 0]]
-
-			#B = np.array([self.previous_position[0], self.previous_position[1], self.previous_position[2], consts[0][3]*-1, consts[1][3]*-1, consts[2][3]*-1])
-			B = np.array([self.current_position[0], self.current_position[1], self.current_position[2], consts[0][3]*-1, consts[1][3]*-1, consts[2][3]*-1])
-
-
-		# print("B = ", B)
-
-		# print("A -1 = ", np.linalg.inv(A))
-
-		# A = 
-		# [1  0  0  a1 a2 a3
-		#  0  1  0  b1 b2 b3
-		#  0  0  1  c1 c2 c3
-		#  a1 b1 c1  0 0  0
-		#  a2 b2 c2  0 0  0
-		#  a3 b3 c3  0 0  0 ]
-
-		# if len(prim_list) == 0:
-		# 	x_godobj = self.god_pos_prev
-		# else:
-		x_godobj = np.dot(np.linalg.inv(A), B)
-
-		#print("HIP POSITION ", self.current_position)
-		# print("PRIM LIST FINAL ", prim_list)
-		# print("GO POSITION ", x_godobj)
-
-		return np.array([x_godobj[0], x_godobj[1], x_godobj[2]])
-
-	def calculateForce(self):
-		k = [17, 17, 17]
-		
-		for i in range(0, 3):
-			self.rendered_force[i] = [k[i]*(self.god_pos[i] - self.current_position[i])]
-
-
-
-
-if __name__ == '__main__':
-	model = ConcaveCube()
-	hip = HapticInterfacePoint(model)
-	hip.active_planes =[1,2]
-	# hip.previous_position = [0.08, 1.27, 2.12]
-	# hip.previous_position = [0.08, 1.3, 2.12]
-	# hip.previous_position = [0.08, 1.24, 2.12]
-	# hip.current_position = [0.08, 1.27, 2.12]
-	hip.previous_position = [0.0, 0.0, 0.0]
-	hip.current_position = [0.68, 1.63, 2.3]
-
-	go = hip.calculateGod(hip.active_planes)
-	print(go)
-
-
-
-
-
--- scripts/main.py
+++ scripts/main.py
@@ -13,79 +13,79 @@ from modelObject import *
 import pywavefront
 import time
 
-
-
+## This is the main function that sets up the whole simulation. We create a rendering object, model object, a HIP object, 
+## and a checker object that we will use inside our while loop.
 
 
 if __name__ == '__main__':
 
-	gl = render_environment()
+	#We tested a lot of objects. The bunny and tree were both poorly formed, and the bunny took a really long time to 
+	# render.
 
 	# model = ModelObject('dodecahedron.obj')
 	# model = ModelObject('bunny.obj')
 	# model = ModelObject('tree.obj')
 	# model = ModelObject('actual_cube.obj')
 	# model = ModelObject('hex_prism.obj')
-	model = ModelObject('icosahedron.obj')
-
 	# model = Cube()
 	# model = ConcaveCube()
-	# model = ConcavePrism() ######## THIS OUR GUY
 	# model = Pyramid(((1,0,0),(2, 2, 0),(1,2,0),(2, 1, 2)))
 
 
-	checker = CollisionChecker(model) # From checker.py
+	#2 model objects we used for the demo
+
+	model = ModelObject('icosahedron.obj')
+	# model = ConcavePrism() ######## THIS OUR GUY
+
+
+	# starting points for each object so we don't start inside the object
 
 	## Convex Vertex:
 	pointVertex = (0.5, 1.35, 0)
 	## Concave Vertex:
 	# pointVertex = (2, 1.3, 6.0)
-
-
+	
+	# Creating the rendering environment object and creating a rigid model as well as the haptic interface point to render
+	gl = render_environment()
 	gl.createStaticObj(model.vertices, model.edges, model.faces)
 	gl.createHIP(pointVertex)
-	hip = HapticInterfacePoint(model, initial_position = pointVertex)
 
-	# run = gl.render(collided_faces, hip) # WE DON'T NEED THIS ANYMORE???
+	# Creating a HIP and checker object
+	hip = HapticInterfacePoint(model, initial_position = pointVertex)
+	checker = CollisionChecker(model) # From checker.py
+
 
 	run = True
 	collision_time = 0
 	collided_faces = []
-
 	penetrated = False
 	
+	# The major while loop where we do all the work, as long as we haven't paused the render environment (by hitting 'p' or 'esc')
 	while run == True:
 		
 		# Get user input (arrow keys)
 		transformation = gl.userInput(hip)
 		
 		# Creating "toggle" whether we are inside object or not
-		if penetrated:
-									# print("HAS COLLIDED ", hip.has_collided)
-									# if time.time() - collision_time < 0.02:
-									# 	collision_time = time.time()
-									# else:
+		if penetrated:									
 			## If colliding for the second time, we are NOT IN THE OBJECT
 			if hip.inside_object == True:
 				hip.inside_object = False
 				hip.active_planes = []
 				print("\n**** EXITED OBJECT ****\n")
-				# collision_time = 0
+
 			## If colliding for first time, we are IN THE OBJECT
 			else:
 				hip.inside_object = True
 				hip.active_planes = collided_faces
 				print("\n**** ENTERED OBJECT ****\n")
-				# collision_time = time.time()
 
+		# Updating both the HIP and the god object positions (major calculations here)
 		hip.updatePos(transformation)
 
+		# After calculations, checking if we are penetrating the object
 		penetrated, collided_faces = checker.detectCollision(model.faces, hip.current_position, hip.previous_position, False) # returns a boolean and a list of primitives (indices of the face list)
-		# print("COLLISION WITH OBJECT RETURNED ", is_coll, hip.current_position, hip.previous_position, hip.god_object_pos)
-
-		
-
-		# run = gl.render(collided_faces, hip)
+	
+		# Render all the things we have to render, now with updating information
 		run = gl.render(hip.active_planes, hip) # Now keeping active plane highlighted
 		
-
